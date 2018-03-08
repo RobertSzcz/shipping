9810c71683d8e813aff19277c74e77672c2cb7ce
Robert <robert.szczygiel22@gmail.com>
2018-03-08 11:04:14 +0100

    Add change_vehicle_position test

--- test/driver/driver_handler_test.exs
+++ test/driver/driver_handler_test.exs
@@ -4,11 +4,19 @@ defmodule Shipping.Driver.DriverHandlerTest do
   alias Shipping.Driver.DriverHandler
 
   alias Shipping.Driver.Events.LoadRequestSent
+  alias Shipping.Driver.Events.VehiclePositionChanged
 
   test "send_load_request returns correct event" do
     {:ok, event} = DriverHandler.send_load_request("driver_id", "load_id")
 
     assert %LoadRequestSent{driver_id: "driver_id", load_id: "load_id", uuid: _, timestamp: _} =
+             event
+  end
+
+  test "change_vehicle_position returns correct event" do
+    {:ok, event} = DriverHandler.change_vehicle_position(%{lat: 10.0, lng: 10.0, driver_id: "driver_id"})
+
+    assert %VehiclePositionChanged{lat: 10.0, lng: 10.0, driver_id: "driver_id", uuid: _, timestamp: _} =
              event
   end
 end
